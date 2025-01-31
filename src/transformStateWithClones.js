78e8bc2a7d823620009bde2c59de3f15b0626ccb
pavlosoletskyy <165999040+pavlosoletskyy@users.noreply.github.com>
2025-01-31 08:59:28 +0200

    Update transformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,23 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
+    let currentState = { ...state };
+    let result = [];
+    for (const action of actions) {
+        switch (action.type) {
+            case 'clear':
+                currentState = {};
+                break;
+            case 'addProperties':
+                currentState = { ...currentState, ...action.extraData };
+                break;
+            case 'removeProperties':
+                currentState = Object.fromEntries(
+                    Object.entries(currentState).filter(([key]) => !action.keysToRemove.includes(key))
+                );
+                break;
+        }
+        result.push({ ...currentState });
+    }
+    return result;
 }
-
-module.exports = transformStateWithClones;
